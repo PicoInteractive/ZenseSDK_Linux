cfc7a2037c15836ac431e0d9c18131e7ac984b50
David Gaxiola <david@pico-interactive.com>
2019-07-17 12:34:10 -0700

    Added Ubuntu 18.04 files.

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-ConfigPath=$HOME/.config/PicoZense
-
-if [ ! -d $ConfigPath ]; then
-
-  mkdir $ConfigPath
-
+# Check if user is root/running with sudo
+if [ `whoami` != root ]; then
+    echo Please run this script with sudo
+    exit
 fi
 
-cp -rf ./Config $ConfigPath
+ORIG_PATH=`pwd`
+cd `dirname $0`
+SCRIPT_PATH=`pwd`
+cd $ORIG_PATH
+
+if [ "`uname -s`" != "Darwin" ]; then
+    # Install UDEV rules for USB device
+    cp ${SCRIPT_PATH}/0660-picozense-usb.rules /etc/udev/rules.d/0660-picozense-usb.rules
+    cp -d ${SCRIPT_PATH}/Thirdparty/opencv-3.4.1/lib/x64/* /usr/lib/x86_64-linux-gnu/
+fi
